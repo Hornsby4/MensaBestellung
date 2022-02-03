9b70107240f1215ba6b46a54abd84d3a740d9927
D-Pucher <86948919+D-Pucher@users.noreply.github.com>
2022-02-03 11:16:32 +0100

    better errors, refactoring
    better error handling and messages, code refactoring

--- MensaBestellung/UserPageFoodExchange.aspx.designer.cs
+++ MensaBestellung/UserPageFoodExchange.aspx.designer.cs
@@ -42,22 +42,22 @@ namespace MensaBestellung
         protected global::System.Web.UI.WebControls.Label lbl_exchangeAndName;
 
         /// <summary>
-        /// btn_saveExchangeFoodOrder control.
+        /// btn_saveOrder control.
         /// </summary>
         /// <remarks>
         /// Auto-generated field.
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
-        protected global::System.Web.UI.WebControls.Button btn_saveExchangeFoodOrder;
+        protected global::System.Web.UI.WebControls.Button btn_saveOrder;
 
         /// <summary>
-        /// btn_throwAwayExchangeFoodChanges control.
+        /// btn_cancelChanges control.
         /// </summary>
         /// <remarks>
         /// Auto-generated field.
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
-        protected global::System.Web.UI.WebControls.Button btn_throwAwayExchangeFoodChanges;
+        protected global::System.Web.UI.WebControls.Button btn_cancelChanges;
 
         /// <summary>
         /// btn_goBack control.
